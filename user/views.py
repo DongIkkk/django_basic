03e45a6b5cfb3592a72a400ec88b47504223269f
DongIkkk <w8h0412@gmail.com>
2022-09-28 11:47:56 +0900

    로그인 이후 기능 다듬기

--- user/views.py
+++ user/views.py
@@ -2,12 +2,18 @@ from django.shortcuts import render, redirect
 from .models import UserModel
 from django.http import HttpResponse
 from django.contrib.auth import get_user_model  # 사용자가 데이터베이스 안에 있는지 검사하는 함수
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 def sign_up_view(request):
     if request.method == 'GET':
-        return render(request, 'user/signup.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signup.html')
     elif request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
@@ -32,11 +38,22 @@ def sign_in_view(request):
         password = request.POST.get('password', None)
 
         # 입력받은 유저네임과 패스워드가 /  데이터베이스의 같은 유저네임 가진 모델이 존재한다면 / 로그인
-        me = UserModel.objects.get(username=username)
-        if me.password == password:
-            request.session['user'] = me.username
-            return HttpResponse(me.username)
+        me = auth.authenticate(request, username=username, password=password)
+        if me is not None:
+            auth.login(request, me)
+            return redirect('/')
         else:
             return redirect('/sign-in')
     elif request.method == 'GET':
-        return render(request, 'user/signin.html')
+        user = request.user.is_authenticated
+        if user:
+            return redirect('/')
+        else:
+            return render(request, 'user/signin.html')
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
